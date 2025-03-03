a97048147717cb516b022b9ca02db5bb535f7b2f
egitmen <egitmen@ZBS0>
2025-03-03 15:20:54 +0300

    Tables added Commit

---
+++ LibraryManagement/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryManagement.Models;
+
+public class Author
+{
+
+  
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string SurName { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public ICollection<Book> Books { get; set; }
+
+
+
+}
--- LibraryManagement/Models/Book.cs
+++ LibraryManagement/Models/Book.cs
@@ -2,12 +2,21 @@
 
 public class Book
 {
+
     public int Id { get; set; }
     public string Title { get; set; }
     public string Isbn { get; set; }
-    public string Category { get; set; }
-    public string Author { get; set; }
 
     public int Page { get; set; }
     public double Price { get; set; }
+
+
+    // Navigation Property
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+
+    public int AuthorId { get; set; }
+    public Author Author { get; set; }
 }
